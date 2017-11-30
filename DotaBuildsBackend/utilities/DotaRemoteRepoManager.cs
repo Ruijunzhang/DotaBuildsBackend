b12af103c78d51684ebb630a4cba7d5b647d2349
RUIJUN\ruijun <ruijunzhang@outlook.com>
2017-11-30 22:31:23 +0100

    project set up

---
+++ DotaBuildsBackend/utilities/DotaRemoteRepoManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotaBuildsBackend.utilities
+{
+    public class DotaRemoteRepoManager
+    {
+
+        public String GetApiRemoteUrl()
+        {
+            return "";
+        } 
+    }
+}
